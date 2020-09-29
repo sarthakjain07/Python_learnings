160ccd518b1328553d3400152356a4bc0299a6f2
Sarthak Jain <sarthakjain7@outlook.com>
2020-09-29 19:50:13 +0530

    Updated Folder

---
+++ Unit_1/Solutions & Programs/Problem_1.py
@@ -0,0 +1,9 @@
+'''
+This problem is a solution of
+Problem 1 of Practice set
+'''
+
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.''')
